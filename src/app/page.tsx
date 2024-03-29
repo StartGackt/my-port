8b06b8d73acf4e76dcfc597ebd8ef0add0aaad22
Your Name <thestackdev@gmail.com>
2024-03-29 10:07:37 +0700

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import Skills from "./components/Skills";
 
 import Email from "./components/Email";
 
-
+// test 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212] ">
